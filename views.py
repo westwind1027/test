6898c493a3bb74b483933a3debc8642f0f044f92
westwind <768523914@qq.com>
2020-07-05 19:42:51 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def index(request):
     return HttpRespose('index')
+
+
+def login(request):
+    return redirect('index')
